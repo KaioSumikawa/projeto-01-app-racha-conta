c643cbdd522a2677d22980d7e7ad3467a27b38f3
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-06-24 23:04:12 +0900

    feat: commit-05

--- conta.py
+++ conta.py
@@ -3,4 +3,7 @@ conta_total = float(input('valor total da conta?: '))
 # já tinha deixado float por costume 
 pessoas_mesa = int(input('quantas pessoas na mesa?: '))
 
+valor_basico = conta_total / pessoas_mesa
+ 
+
 
