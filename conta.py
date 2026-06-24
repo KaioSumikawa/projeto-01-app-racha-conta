9b3b5e8e2c52d18f6b77f28cdccfa03c711af012
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-06-24 23:10:41 +0900

    feat: commit-08

--- conta.py
+++ conta.py
@@ -4,8 +4,10 @@ conta_total = float(input('valor total da conta?: '))
 pessoas_mesa = int(input('quantas pessoas na mesa?: '))
 
 garcom = conta_total * 0.10
-
 valor_basico = conta_total / pessoas_mesa
+
 print(f'Cada pessoa deve pagar: R$ {valor_basico:.2f}')
+print(f'Valor do garcom: R$ {garcom:.2f}')
+
 
 
